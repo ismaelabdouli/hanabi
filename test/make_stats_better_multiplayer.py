f6dd9e648c558017b141b23b611eca5d654a5025
Ismael ABDOULI <ismael.abdouli@ensta-paristech.fr>
2019-05-24 18:24:45 +0200

    IA équivalente à Player better mais mieux faite

---
+++ test/make_stats_better_multiplayer.py
@@ -0,0 +1,30 @@
+import hanabi
+import hanabi.ai
+
+import statistics
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+L=[]
+game=hanabi.Game(5)
+import hanabi.ai_vraiment_mieux_multiplayer as new_ai
+
+for i in range(1000):
+    game.reset()
+    game=hanabi.Game(5)
+    game.quiet=True
+    ai=new_ai.Player_better(game)
+    game.ai = ai
+    game.run()
+    L.append(game.score)
+
+print(statistics.mean(L))
+#X=[]
+# for i in range(max(L)+1):
+#     X.append[L.count(i)]
+# plt.plot(X,range(max(L)+1))
+plt.hist(L)
+plt.title('Statistiques pour Player better multijoueur')
+plt.savefig('stat_better_multijoueur.png')
+plt.show()
